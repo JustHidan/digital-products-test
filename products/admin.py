00e3e64628c071f5b4301a1609b8f2ee6ef03094
Hidan <Hidan@code.com>
2024-10-06 20:44:13 +0330

    admin

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
+from products.models import Product,File,Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['parent' , 'title','created_time']
+    list_filter = ['parent']
+    search_fields = ['title']
+
+class FileInlineAdmin (admin.StackedInline):
+    model = File
+    fields = ['title','file','is_enable']
+    extra = 0
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ['title','category','created_time']
+    list_filter = ['created_time']
+    search_fields = ['title']
+    inlines = [FileInlineAdmin]
 
 # Register your models here.
